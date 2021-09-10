8037b08f288a24ddba7e191b9d40b0156ffaba74
Nanmaran Anbupalam <nanmaran.anbu@gmail.com>
2021-09-10 19:39:09 +1000

    refactor: Added description to the main function.

--- bikeshare_revised.py
+++ bikeshare_revised.py
@@ -270,7 +270,7 @@ def get_raw_data(df):
     """
     Asks user if they want to view the first 5 lines of filtered raw data. If they select yes, then it asks if they want
     to view the next 5 and so on until they say no.
-    """ 
+    """
     print ('Would you like to see the first 5 lines of raw data?')
     raw_data = input('yes/no\n')
     raw_data=raw_data.lower()
@@ -295,6 +295,9 @@ def get_raw_data(df):
         return
 
 def main():
+    """
+    Calling all the functions
+    """
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
@@ -305,6 +308,9 @@ def main():
         user_stats(df)
         get_raw_data(df)
 
+        """
+        Giving the users an option to restart the entire program to look at other data.
+        """
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         restart = restart.lower()
         if restart.lower() != 'yes':
